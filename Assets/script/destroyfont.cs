ffe8b88916e6c50ce1b1d7c1804cafcacf41e94d
soyeone <35295141+soyeone@users.noreply.github.com>
2018-01-21 19:24:38 +0900

    update
    - add objects(light, book and bin)
    - add gestures(stop,control weather)
    - orgnize scripts and files

---
+++ Assets/script/destroyfont.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyfont : MonoBehaviour {
+
+    //띄운 파티클 나중에 없앰
+    // controll particle
+
+    public ParticleSystem particle;
+
+    public void pariclestop() {
+
+        particle = gameObject.GetComponent<ParticleSystem>();
+        particle.enableEmission = false;
+    }
+
+    public void particlestart() {
+
+        particle = gameObject.GetComponent<ParticleSystem>();
+        particle.enableEmission = true;
+
+    }
+}
